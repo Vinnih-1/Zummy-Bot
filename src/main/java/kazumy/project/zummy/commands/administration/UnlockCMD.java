da93c56be98b9896a73e1f839a184de3d63211a1
Vinícius <vinifantal@hotmail.com>
2021-10-11 03:23:01 -0300

    Comando `unlock` finalizado permissão `MANAGE_CHANNEL`

---
+++ src/main/java/kazumy/project/zummy/commands/administration/UnlockCMD.java
@@ -0,0 +1,27 @@
+package kazumy.project.zummy.commands.administration;
+
+import kazumy.project.zummy.commands.BaseCommand;
+import lombok.val;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
+
+public class UnlockCMD extends BaseCommand {
+
+	public UnlockCMD() {
+		super("unlock", "", "[%s]unlock", Permission.MANAGE_CHANNEL);
+	}
+
+	@Override
+	public void execute(Member member, Message message) {
+		val role = message.getGuild().getRoleById("896553346738057226");
+		
+		message.getTextChannel().getManager().putPermissionOverride(role, 2048, 0).queue();;
+		message.getTextChannel().sendMessage(String.format("Chat destravado por <@%s>", message.getAuthor().getId())).queue();
+	}
+
+	@Override
+	public void buttonClickEvent(ButtonClickEvent event) {
+	}
+}
