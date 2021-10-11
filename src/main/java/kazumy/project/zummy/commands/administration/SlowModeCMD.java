eef4ef965a2c493817a95139c6fe103e63b1fbaa
Vinícius <vinifantal@hotmail.com>
2021-10-11 03:18:51 -0300

    Comando `slowmode` finalizado permissão `MANAGE_CHANNEL`

--- src/main/java/kazumy/project/zummy/commands/administration/SlowModeCMD.java
+++ src/main/java/kazumy/project/zummy/commands/administration/SlowModeCMD.java
@@ -1,18 +1,34 @@
 package kazumy.project.zummy.commands.administration;
 
 import kazumy.project.zummy.commands.BaseCommand;
+import kazumy.project.zummy.listener.EventListener;
+import lombok.val;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 
 public class SlowModeCMD extends BaseCommand {
 
-	public SlowModeCMD(String name, String aliases, String usage, Permission permission) {
-		super(name, aliases, usage, permission);
+	public SlowModeCMD() {
+		super("slowmode", "", "[%s]slowmode [Tempo]", Permission.MANAGE_CHANNEL);
 	}
 
 	@Override
-	public void execute(Member member, String[] args) {
+	public void execute(Member member, Message message) {
+		val args = message.getContentRaw().split(" ");
+		
+		if (args.length <= 1) {
+			message.reply(String.format(this.getUsage(), EventListener.PREFIX)).queue();
+			return;
+		}
+		
+		try { Integer.parseInt(args[1]); } catch (NumberFormatException e) {
+			message.reply(String.format(this.getUsage(), EventListener.PREFIX)).queue();;
+			return;
+		}
+		message.getTextChannel().getManager().setSlowmode(Integer.parseInt(args[1])).queue();;
+		message.getTextChannel().sendMessage(String.format("Modo lento habilitado para %s segundos", args[1])).queue();
 	}
 
 	@Override
