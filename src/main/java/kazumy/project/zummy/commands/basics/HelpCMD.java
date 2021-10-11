8fd9c51ca958b31381ccb61c66b4f7e17feb18ff
Vinícius <vinifantal@hotmail.com>
2021-10-11 03:19:32 -0300

    Comando `help` finalizado permissão `null`

--- src/main/java/kazumy/project/zummy/commands/basics/HelpCMD.java
+++ src/main/java/kazumy/project/zummy/commands/basics/HelpCMD.java
@@ -1,18 +1,37 @@
 package kazumy.project.zummy.commands.basics;
 
+import java.awt.Color;
+
 import kazumy.project.zummy.commands.BaseCommand;
-import net.dv8tion.jda.api.Permission;
+import kazumy.project.zummy.listener.EventListener;
+import lombok.val;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 
+@SuppressWarnings("deprecation")
 public class HelpCMD extends BaseCommand {
 
-	public HelpCMD(String name, String aliases, String usage, Permission permission) {
-		super(name, aliases, usage, permission);
+	public HelpCMD() {
+		super("help", "ajuda", "[%s]help", null);
 	}
 
 	@Override
-	public void execute(Member member, String[] args) {
+	public void execute(Member member, Message message) {
+		val embed = new EmbedBuilder();
+		embed.setColor(Color.YELLOW);
+
+		embed.setTitle("Comandos Atuais");
+		embed.setThumbnail("https://cdn.discordapp.com/icons/832601856403701771/94a08bab250ed87791c68bec4e7a4013.png");
+		
+		val commands = new EventListener().getCommands();
+		val usages = new StringBuilder();
+
+		commands.stream().filter(c -> c.getPermission() == null).forEach(c -> usages.append("`").append(String.format(c.getUsage(), EventListener.PREFIX)).append("`").append("\n"));
+		
+		embed.setDescription(usages.toString());
+		message.reply(embed.build()).queue();
 	}
 
 	@Override
