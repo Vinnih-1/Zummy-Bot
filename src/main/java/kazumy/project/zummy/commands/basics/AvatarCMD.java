8e7ad3edf31de5b84fe8d743e9210b9d8dd7a4a5
Vinícius <vinifantal@hotmail.com>
2021-10-11 03:19:19 -0300

    Comando `avatar` finalizado permissão `null`

--- src/main/java/kazumy/project/zummy/commands/basics/AvatarCMD.java
+++ src/main/java/kazumy/project/zummy/commands/basics/AvatarCMD.java
@@ -1,18 +1,39 @@
 package kazumy.project.zummy.commands.basics;
 
 import kazumy.project.zummy.commands.BaseCommand;
-import net.dv8tion.jda.api.Permission;
+import lombok.val;
+import lombok.var;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 
+@SuppressWarnings("deprecation")
 public class AvatarCMD extends BaseCommand {
 
-	public AvatarCMD(String name, String aliases, String usage, Permission permission) {
-		super(name, aliases, usage, permission);
+	public AvatarCMD() {
+		super("avatar", "", "[%s]avatar [Membro]", null);
 	}
 
 	@Override
-	public void execute(Member member, String[] args) {
+	public void execute(Member member, Message message) {
+		val args = message.getContentRaw().split(" ");
+		val embed = new EmbedBuilder();
+		var target = message.getMentionedMembers().isEmpty() ? null : message.getMentionedMembers().get(0).getUser();
+		
+		if (target == null) target = member.getUser();
+		
+		embed.setTitle(target.getName());
+		embed.setDescription("**Avatar**");
+		embed.setFooter("zPluginS © Todos os direitos reservados.", "https://cdn.discordapp.com/icons/832601856403701771/94a08bab250ed87791c68bec4e7a4013.png");
+		
+		if (args.length <= 1) {
+			embed.setImage(member.getUser().getAvatarUrl().concat("?size=256"));
+			message.reply(embed.build()).queue();
+		} else if (!message.getMentionedMembers().isEmpty()) {
+			embed.setImage(message.getMentionedMembers().get(0).getUser().getAvatarUrl().concat("?size=256"));
+			message.reply(embed.build()).queue();
+		}
 	}
 
 	@Override
