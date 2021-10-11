0939ddc99b0ddc3184486c70936e6f487857209b
Vinícius <vinifantal@hotmail.com>
2021-10-11 03:21:51 -0300

    Debug removido + Setagem de presença e adição do `EventListener`

--- src/main/java/kazumy/project/zummy/discord/Discord.java
+++ src/main/java/kazumy/project/zummy/discord/Discord.java
@@ -3,12 +3,16 @@ package kazumy.project.zummy.discord;
 import javax.security.auth.login.LoginException;
 
 import kazumy.project.zummy.configuration.Config;
+import kazumy.project.zummy.listener.EventListener;
 import lombok.Data;
 import lombok.Getter;
 import lombok.SneakyThrows;
 import lombok.val;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 @Getter
 @Data(staticConstructor = "of")
@@ -20,19 +24,17 @@ public class Discord {
 	@SneakyThrows
 	public Discord startBot() {
 		val token = String.valueOf(config.getJson().get("token"));
-		System.out.println(token);
+		val status = String.valueOf(config.getJson().get("status"));
 		
 		try {
-			bot = JDABuilder.createDefault(token).build().awaitReady();
+			bot = JDABuilder.createDefault(token).enableIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_MESSAGES).build().awaitReady();
+			bot.addEventListener(new EventListener());
 			
+			bot.getPresence().setPresence(OnlineStatus.ONLINE, Activity.playing(status));
 		} catch (LoginException e) {
 			System.out.println("[Zummy] Algo de errado com o token provido.");
 		}
 		
 		return this;
-	}
-	
-	public void registerEvents(Object... listeners) {
-		bot.addEventListener(listeners);
 	}
 }
