3a1b8f3bedcf5b18b6c714649eff842af3d1416f
Vinícius <vinifantal@hotmail.com>
2021-10-11 03:19:47 -0300

    Comando `ping` finalizado permissão `null`

--- src/main/java/kazumy/project/zummy/commands/basics/PingCMD.java
+++ src/main/java/kazumy/project/zummy/commands/basics/PingCMD.java
@@ -1,18 +1,19 @@
 package kazumy.project.zummy.commands.basics;
 
 import kazumy.project.zummy.commands.BaseCommand;
-import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 
 public class PingCMD extends BaseCommand {
 
-	public PingCMD(String name, String aliases, String usage, Permission permission) {
-		super(name, aliases, usage, permission);
+	public PingCMD() {
+		super("ping", "", "[%s]ping", null);
 	}
 
 	@Override
-	public void execute(Member member, String[] args) {
+	public void execute(Member member, Message message) {
+		message.reply("Meu ping atual é de " + message.getJDA().getGatewayPing() + "ms").queue();
 	}
 
 	@Override
