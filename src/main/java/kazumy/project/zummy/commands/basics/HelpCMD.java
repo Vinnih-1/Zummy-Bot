6bf5b9b081f241cec3c118366c856f1ec6ccf835
Vinícius <vinifantal@hotmail.com>
2021-11-17 17:07:30 -0300

    Comando `help` com reflection

--- src/main/java/kazumy/project/zummy/commands/basics/HelpCMD.java
+++ src/main/java/kazumy/project/zummy/commands/basics/HelpCMD.java
@@ -1,10 +1,14 @@
 package kazumy.project.zummy.commands.basics;
 
 import java.awt.Color;
+import java.util.ArrayList;
+
+import org.reflections.Reflections;
 
 import kazumy.project.zummy.commands.BaseCommand;
 import kazumy.project.zummy.listener.EventListener;
 import lombok.val;
+import lombok.var;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
@@ -25,7 +29,16 @@ public class HelpCMD extends BaseCommand {
 		embed.setTitle("Comandos Atuais");
 		embed.setThumbnail("https://cdn.discordapp.com/icons/832601856403701771/94a08bab250ed87791c68bec4e7a4013.png");
 		
-		val commands = new EventListener().getCommands();
+		val classes = new Reflections("kazumy.project.zummy.commands").getSubTypesOf(BaseCommand.class);
+		var commands = new ArrayList<BaseCommand>(); 
+		
+		classes.forEach(c -> {
+			try {
+				commands.add(c.newInstance());
+			} catch (InstantiationException | IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		});
 		val usages = new StringBuilder();
 
 		commands.stream().filter(c -> c.getPermission() == null).forEach(c -> usages.append("`").append(String.format(c.getUsage(), EventListener.PREFIX)).append("`").append("\n"));
